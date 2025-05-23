61c39fcb76894ab54b45ce6c516d1ac536a8186b
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-23 23:46:36 +0700

    feat : create index servlet to handle index.jsp

---
+++ src/main/java/controller/IndexServlet.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.User;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getSession().getAttribute("user") != null) {
+            String role = ((User) request.getSession().getAttribute("user")).getRole();
+            switch (role) {
+                case "admin":
+                    response.sendRedirect("views/dashboard/admin-dashboard.jsp");
+                    break;
+                case "hr":
+                    response.sendRedirect("views/dashboard/hr-dashboard.jsp");
+                    break;
+                case "employee":
+                    response.sendRedirect("views/dashboard/employee-dashboard.jsp");
+                    break;
+            }
+        } else {
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
