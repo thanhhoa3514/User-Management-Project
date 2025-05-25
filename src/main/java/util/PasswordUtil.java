0266ebf640d72a29993a810b5ba5ddc24fc4a656
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-25 21:43:41 +0700

    feat : create a hash password by bcrypt passord library

---
+++ src/main/java/util/PasswordUtil.java
@@ -0,0 +1,13 @@
+package util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+	public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String password, String hashed) {
+        return BCrypt.checkpw(password, hashed);
+    }
+}
